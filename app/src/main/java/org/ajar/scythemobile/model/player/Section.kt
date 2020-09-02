a6542b2894a2349e7fc710a9a6411b91c0c4714a
Matt Stockbridge <Matthew.Stockbridge@sap.com>
2020-09-02 16:11:05 -0700

    Upping some versions and adding destination information into the Section model.

--- app/src/main/java/org/ajar/scythemobile/model/player/Section.kt
+++ app/src/main/java/org/ajar/scythemobile/model/player/Section.kt
@@ -1,5 +1,6 @@
 package org.ajar.scythemobile.model.player
 
+import androidx.navigation.NavDirections
 import org.ajar.scythemobile.CapitalResourceType
 import org.ajar.scythemobile.NaturalResourceType
 import org.ajar.scythemobile.R
@@ -8,11 +9,23 @@ import org.ajar.scythemobile.data.PlayerMatData
 import org.ajar.scythemobile.data.ScytheDatabase
 import org.ajar.scythemobile.model.PlayerInstance
 import org.ajar.scythemobile.model.entity.UnitType
+import org.ajar.scythemobile.ui.PassOffFragmentDirections
+import org.ajar.scythemobile.ui.StartTurnFragmentDirections
+import org.ajar.scythemobile.ui.bolster.BolsterFragmentDirections
+import org.ajar.scythemobile.ui.build.BuildFragmentDirections
+import org.ajar.scythemobile.ui.deploy.DeployFragmentDirections
+import org.ajar.scythemobile.ui.enlist.EnlistFragmentDirections
+import org.ajar.scythemobile.ui.move.MoveFragmentDirections
+import org.ajar.scythemobile.ui.produce.ProduceFragmentDirections
+import org.ajar.scythemobile.ui.trade.TradeFragmentDirections
+import org.ajar.scythemobile.ui.upgrade.UpgradeFragmentDirections
 
 class Section(val topRowAction: TopRowAction, val bottomRowAction: BottomRowAction)
 
 interface PlayerMatAction {
     val fragmentNav: Int
+    val actionInto: NavDirections
+    val actionOutOf: NavDirections
     val playerInstance: PlayerInstance
     val playerMatData: PlayerMatData
         get() = playerInstance.playerData.playerMat
@@ -23,6 +36,8 @@ interface PlayerMatAction {
 sealed class TopRowAction(override val playerInstance: PlayerInstance) : PlayerMatAction {
     class MoveOrGain(playerInstance: PlayerInstance) : TopRowAction(playerInstance) {
         override val fragmentNav: Int = R.id.nav_move
+        override val actionInto: NavDirections = StartTurnFragmentDirections.actionNavStartToNavMove()
+        override val actionOutOf: NavDirections = MoveFragmentDirections.actionNavMoveToNavPassOff()
 
         private val data = playerMatData.moveGainSection
 
@@ -33,6 +48,8 @@ sealed class TopRowAction(override val playerInstance: PlayerInstance) : PlayerM
     }
     class Trade(playerInstance: PlayerInstance) : TopRowAction(playerInstance) {
         override val fragmentNav: Int = R.id.nav_trade
+        override val actionInto: NavDirections = StartTurnFragmentDirections.actionNavStartToNavTrade()
+        override val actionOutOf: NavDirections = TradeFragmentDirections.actionNavTradeToNavPassOff()
 
         private val data = playerMatData.tradeSection
 
@@ -43,6 +60,8 @@ sealed class TopRowAction(override val playerInstance: PlayerInstance) : PlayerM
     }
     class Produce(playerInstance: PlayerInstance) : TopRowAction(playerInstance) {
         override val fragmentNav: Int = R.id.nav_produce
+        override val actionInto: NavDirections = StartTurnFragmentDirections.actionNavStartToNavProduce()
+        override val actionOutOf: NavDirections = ProduceFragmentDirections.actionNavProduceToNavPassOff()
 
         private val data = playerMatData.produceSection
 
@@ -61,6 +80,8 @@ sealed class TopRowAction(override val playerInstance: PlayerInstance) : PlayerM
     }
     class Bolster(playerInstance: PlayerInstance) : TopRowAction(playerInstance) {
         override val fragmentNav: Int = R.id.nav_bolster
+        override val actionInto: NavDirections = StartTurnFragmentDirections.actionNavStartToNavBolster()
+        override val actionOutOf: NavDirections = BolsterFragmentDirections.actionNavBolsterToNavPassOff()
 
         private val data = playerMatData.bolsterSection
 
@@ -74,6 +95,8 @@ sealed class TopRowAction(override val playerInstance: PlayerInstance) : PlayerM
 sealed class BottomRowAction(override val playerInstance: PlayerInstance, val startingCost: Int, val costBottom: Int, val coins: Int) : PlayerMatAction {
     class Upgrade(playerInstance: PlayerInstance, startingCost: Int, costBottom: Int, coins: Int) : BottomRowAction(playerInstance, startingCost, costBottom, coins) {
         override val fragmentNav: Int = R.id.nav_upgrade
+        override val actionInto: NavDirections = PassOffFragmentDirections.actionNavPassOffToNavUpgrade()
+        override val actionOutOf: NavDirections = UpgradeFragmentDirections.actionNavUpgradeToNavEnd()
 
         private val data = playerMatData.upgradeSection
 
@@ -89,6 +112,8 @@ sealed class BottomRowAction(override val playerInstance: PlayerInstance, val st
     }
     class Deploy(playerInstance: PlayerInstance, startingCost: Int, costBottom: Int, coins: Int) : BottomRowAction(playerInstance, startingCost, costBottom, coins) {
         override val fragmentNav: Int = R.id.nav_deploy
+        override val actionInto: NavDirections = PassOffFragmentDirections.actionNavPassOffToNavDeploy()
+        override val actionOutOf: NavDirections = DeployFragmentDirections.actionNavDeployToNavEnd()
 
         private val data = playerMatData.deploySection
 
@@ -104,6 +129,8 @@ sealed class BottomRowAction(override val playerInstance: PlayerInstance, val st
     }
     class Build(playerInstance: PlayerInstance, startingCost: Int, costBottom: Int, coins: Int) : BottomRowAction(playerInstance, startingCost, costBottom, coins) {
         override val fragmentNav: Int = R.id.nav_build
+        override val actionInto: NavDirections = PassOffFragmentDirections.actionNavPassOffToNavBuild()
+        override val actionOutOf: NavDirections = BuildFragmentDirections.actionNavBuildToNavEnd()
 
         private val data = playerMatData.buildSection
 
@@ -119,6 +146,8 @@ sealed class BottomRowAction(override val playerInstance: PlayerInstance, val st
     }
     class Enlist(playerInstance: PlayerInstance, startingCost: Int, costBottom: Int, coins: Int) : BottomRowAction(playerInstance, startingCost, costBottom, coins) {
         override val fragmentNav: Int = R.id.nav_enlist
+        override val actionInto: NavDirections = PassOffFragmentDirections.actionNavPassOffToNavEnlist()
+        override val actionOutOf: NavDirections = EnlistFragmentDirections.actionNavEnlistToNavEnd()
 
         private val data = playerMatData.enlistSection
 
--- app/src/main/java/org/ajar/scythemobile/ui/ScytheTurnViewModel.kt
+++ app/src/main/java/org/ajar/scythemobile/ui/ScytheTurnViewModel.kt
@@ -1,11 +1,17 @@
 package org.ajar.scythemobile.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavDirections
 import org.ajar.scythemobile.R
+import org.ajar.scythemobile.data.CombatRecord
 import org.ajar.scythemobile.data.ScytheDatabase
 import org.ajar.scythemobile.model.PlayerInstance
 import org.ajar.scythemobile.model.player.Section
 import org.ajar.scythemobile.turn.TurnHolder
+import org.ajar.scythemobile.ui.move.AnswerCombatFragmentDirections
+import org.ajar.scythemobile.ui.move.MoveFragmentDirections
+import org.ajar.scythemobile.ui.move.ResolveCombatFragmentDirections
+import org.ajar.scythemobile.ui.move.StartCombatFragmentDirections
 
 class ScytheTurnViewModel : ViewModel() {
 
@@ -31,13 +37,46 @@ class ScytheTurnViewModel : ViewModel() {
             }
         }
 
+    fun finishSection(current: Int): NavDirections? {
+        return when(current) {
+            R.id.nav_start -> currentSection!!.topRowAction.actionInto
+            currentSection!!.topRowAction.fragmentNav -> {
+                when {
+                    TurnHolder.currentTurn.combatOne?.combatResolved == false -> MoveFragmentDirections.actionNavMoveToNavStartCombat()
+                    TurnHolder.currentTurn.combatTwo?.combatResolved == false -> MoveFragmentDirections.actionNavMoveToNavStartCombat()
+                    TurnHolder.currentTurn.combatThree?.combatResolved == false -> MoveFragmentDirections.actionNavMoveToNavStartCombat()
+                    else -> currentSection!!.topRowAction.actionOutOf
+                }
+            }
+            R.id.nav_start_combat -> StartCombatFragmentDirections.actionNavStartCombatToNavAnswerCombat()
+            R.id.nav_answer_combat -> AnswerCombatFragmentDirections.actionNavAnswerCombatToNavResolveCombat()
+            R.id.nav_resolve_combat -> ResolveCombatFragmentDirections.actionNavResolveCombatToNavMove()
+            R.id.nav_pass_off -> currentSection!!.bottomRowAction.actionInto.also { TurnHolder.commitChanges() }
+            currentSection!!.bottomRowAction.fragmentNav -> currentSection!!.bottomRowAction.actionOutOf.also { TurnHolder.commitChanges() }
+            else -> null
+        }
+    }
+
     fun selectSection(selection: Int) {
         TurnHolder.currentTurn.selection = selection
         ScytheDatabase.turnDao()?.updateTurn(TurnHolder.currentTurn)
     }
 
+    private fun getCombatData(round: Int) : CombatRecord? {
+        return when(round) {
+            1 -> TurnHolder.currentTurn.combatOne
+            2 -> TurnHolder.currentTurn.combatTwo
+            else -> TurnHolder.currentTurn.combatThree
+        }
+    }
+
     private fun determineCombatPhase(round: Int): Int {
-        //TODO: Make this figure out combat phase.
-        return R.id.nav_start_combat
+        val record = getCombatData(round)
+
+        return when {
+            record?.attackerPower == null -> R.id.nav_start_combat
+            record.defenderPower == null -> R.id.nav_answer_combat
+            else -> R.id.nav_resolve_combat
+        }
     }
 }
